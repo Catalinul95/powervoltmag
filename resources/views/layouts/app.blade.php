859f391ad3e6cc11416f2cc203773af95047b162
Catalinul <pocsan95@gmail.com>
2020-03-18 19:27:25 +0200

    some more work 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,13 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .dropdown-menu {
+            width: 300px !important;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
